83a45802eb5e6d871d612cd57db465c612ac8a62
Joe Ainsworth <joe.ainsworth@me.com>
2015-12-07 23:06:39 +0000

    Added additional validation features and looping to the calculator

--- calculator.rb
+++ calculator.rb
@@ -3,25 +3,113 @@
 # perform the operation
 # output the result
 
-Kernel.puts("Welcome to the procedural calculator!")
-
-Kernel.puts("What's the first number?")
-number_1 = Kernel.gets.chomp
-
-Kernel.puts("What's the second number?")
-number_2 = Kernel.gets.chomp
-
-Kernel.puts("What operation would you like to perform?\n[1] Addition [2] Subtraction [3] Multiplication [4] Division")
-operator = Kernel.gets.chomp
-
-if operator == '1'
-  result = number_1.to_i + number_2.to_i
-elsif operation == '2'
-  result = number_1.to_i  - number_2.to_i
-elsif operator == '3'
-  result = number_1.to_i  * number_2.to_i
-else operator
-  result = number_1.to_f / number_2.to_f
+def prompt(msg)
+  Kernel.puts("=> #{msg}")
 end
 
-Kernel.puts("The result is: #{result}")
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+end
+
+def number?(num)
+  integer?(num) || float?(num)
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividng'
+  end
+end
+
+prompt('Welcome to the procedural calculator! Please enter your name:')
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt('Please enter a valid name.')
+  else
+    break
+  end
+end
+
+prompt("Hello #{name}!")
+
+loop do
+  number_1 = ''
+  loop do
+    prompt("What's the first number?")
+    number_1 = Kernel.gets.chomp
+
+    if number?(number_1)
+      break
+    else
+      prompt('Please enter a valid number.')
+    end
+  end
+
+  number_2 = ''
+  loop do
+    prompt("What's the second number?")
+    number_2 = Kernel.gets.chomp
+
+    if number?(number_2)
+      break
+    else
+      prompt('Please enter a valid number.')
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    [1] Addition
+    [2] Subtraction
+    [3] Multiplication
+    [4] Division
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Please enter a valid operation.")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number_1.to_i + number_2.to_i
+           when '2'
+             number_1.to_i - number_2.to_i
+           when '3'
+             number_1.to_i * number_2.to_i
+           when '4'
+             number_1.to_f / number_2.to_f
+           end
+
+  prompt("The result is: #{result}")
+
+  prompt("Would you like to perform another calculation? [y] Yes [n] No")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('Goodbye!')
